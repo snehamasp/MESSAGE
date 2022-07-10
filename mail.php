49a9dda32a9765245a45a803a75180d8b3b13b7c
SNEHA U <93270477+snehamasp@users.noreply.github.com>
2022-07-11 01:13:27 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,11 +7,7 @@ $message= $_POST['message'];
 $to = "$stumail";
 $subject = "Mail From mentor";
 $txt ="Name = ". $name . "\r\n  Phone No. = " . $num . "\r\n Message =" . $message;
-$headers = "From: noreply@github.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
+mail($to,$subject,$txt,$headers);
+or die("Error");
+echo"message sent";
 ?>
